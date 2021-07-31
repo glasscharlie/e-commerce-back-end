ff65d713896c2a41e902fa067debdaecf986f730
Charles Glass <charlie-glass@live.com>
2021-07-30 18:07:27 -0700

    added backend server route functionality and DB table initialization

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:[Category, Tag]
+}).then( product=>{
+    res.json(product);
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+})
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id,{
+    include:[{model:Category}, {model:Tag}]
+}).then(product=>{
+    res.json(product);
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+})
 });
 
 // create new product
@@ -50,7 +66,14 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update({
+
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+    },
+  {
     where: {
       id: req.params.id,
     },
@@ -90,7 +113,16 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where:{
+        id:req.params.id
+    }
+}).then(product=>{
+    res.json(product);
+}).catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+})
 });
 
 module.exports = router;
